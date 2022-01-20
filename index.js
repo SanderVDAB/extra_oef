31b5d3b89ca77cfeaf1f373a44f021a8f552695a
SanderVDAB <sander.vandeputte@vdabcampus.be>
2022-01-20 13:18:51 +0100

    img buttons

--- index.js
+++ index.js
@@ -7,5 +7,15 @@
 // console.log(window.location.href);
 // test.addEventListener("click", () => openSchaar());
 document.getElementById("year").innerHTML = `2022 - ${new Date().getFullYear()}`;
+var main = document.getElementsByTagName("main");
+
+function opmaakWebsite(){
+   
+}
+
+function openspel() {
+   console.log(main.location.href)
+}
+console.log(main)
 
 // window.addEventListener("load", () => test++, console.log(`geladen ${test}`))
